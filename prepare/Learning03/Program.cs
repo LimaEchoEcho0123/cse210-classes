63b10d2afd9cc279e0f18fa8bc306789cd1bd042
LimaEchoEcho0123 <mmmyappers101@gmail.com>
2023-09-28 11:20:07 -0600

    finished

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        
+        Console.WriteLine($"{fraction1.GetFractionString()}\n{fraction1.GetDecimalValue()}" );
+        Console.WriteLine();
+
+
+        Fraction fraction2 = new Fraction(5);
+        
+        Console.WriteLine($"{fraction2.GetFractionString()}\n{fraction2.GetDecimalValue()}" );
+        Console.WriteLine();
+
+
+
+        Fraction fraction3 = new Fraction(3, 4);
+        
+        Console.WriteLine($"{fraction3.GetFractionString()}\n{fraction3.GetDecimalValue()}" );
+        Console.WriteLine();
+
+        Fraction fraction4 = new Fraction(1, 3);
+        
+        Console.WriteLine($"{fraction4.GetFractionString()}\n{fraction4.GetDecimalValue()}" );
+        Console.WriteLine();
+
     }
 }
---
+++ prepare/Learning03/fraction.cs
@@ -0,0 +1,36 @@
+using System;
+
+public class Fraction
+{
+    private int _top;
+    private int _bot;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bot = 1;
+    }
+
+    public Fraction(int Z)
+    {
+        _top = Z;
+        _bot = 1;
+    }
+
+    public Fraction(int top, int bot)
+    {
+        _top = top;
+        _bot = bot;
+    }
+
+    public string GetFractionString()
+    {
+        string text = $"{_top}/{_bot}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bot;
+    }
+}
